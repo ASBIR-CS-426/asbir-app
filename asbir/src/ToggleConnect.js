ba83f76081989bce0a31b4139e2e8fba4d5068dd
Tanner <trichnak72399@gmail.com>
2023-01-31 13:02:42 -0800

    winners

--- asbir/src/ToggleConnect.js
+++ asbir/src/ToggleConnect.js
@@ -1,20 +1,41 @@
 import React, { useState, useEffect} from 'react'
-import { useROS } from 'react-ros'
+import ROSLIB from 'roslib'
+// import { useROS } from 'react-ros'
 
 const ToggleConnect = () => {
-    const { isConnected, topics, url, changeUrl, toggleConnection } = useROS();
+    // let { isConnected, url, changeUrl, toggleConnection } = useROS();
+    let url = '127.0.0.1:9090'
 
     return (
         <div>
             <p>Hello World!</p>
+            <b>Simple connect:  </b><button onClick={() =>{
+              var ros = new ROSLIB.Ros({
+                url : 'ws://localhost:9090'
+              });
+            
+              ros.on('connection', function() {
+                console.log('Connected to websocket server.');
+              });
+            
+              ros.on('error', function(error) {
+                console.log('Error connecting to websocket server: ', error);
+              });
+            
+              ros.on('close', function() {
+                console.log('Connection to websocket server closed.');
+              });
+            }}>Toggle Connect</button>  <br />
           {/* <p>
-            <b>Simple connect:  </b><button onClick={ toggleConnection }>Toggle Connect</button>  <br />
+            <b>Simple connect:  </b><button onClick={() =>{
+              toggleConnection()
+              console.log(url)
+            }}>Toggle Connect</button>  <br />
             <b>ROS url input:  </b><input name="urlInput" defaultValue={ url } onChange={event => changeUrl(event.target.value)} />  <br />
             <b>ROS url to connect to:  </b> {url}  <br />
             <b>Status of ROS:</b> { isConnected ? "connected" : "not connected" }   <br />
             <b>Topics detected:</b><br />
-            { topics.map((topic, i) => <li key={i}>    {topic.path}</li> )}
-          </p> */}
+            { topics.map((topic, i) => <li key={i}>    {topic.path}</li> )} */}
         </div>
     );
 }
