7f2ade7946466333ffbbd457f651591d8229036e
Tanner <trichnak72399@gmail.com>
2023-02-09 12:52:16 -0800

    Camera Feed

--- asbir/src/ToggleConnect.js
+++ asbir/src/ToggleConnect.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect} from 'react'
 import ROSLIB from 'roslib'
 
-const ToggleConnect = () => {
-    // let { isConnected, url, changeUrl, toggleConnection } = useROS();
+let ros = new ROSLIB.Ros({
+  url : 'ws://localhost:9090'
+});
 
-    useEffect(() => {
-    })
+let image_topic = new ROSLIB.Topic({
+  ros: ros, 
+  name: '/CompressedImage',
+  messageType: 'sensor_msgs/CompressedImage'
+});
+
+image_topic.subscribe(function(message) {
+  console.log('Received message on ' + image_topic.name);
+  document.getElementById('my_image').src = "data:image/jpg;base64," + message.data;
+  // image_topic.unsubscribe()
+})
+
+const ToggleConnect = () => {
 
     return (
         <div>
             <p>Hello World!</p>
             <b>Simple connect:  </b><button onClick={() =>{
-              var ros = new ROSLIB.Ros({
-                url : 'ws://localhost:9090'
-              });
             
               ros.on('connection', function() {
                 console.log('Connected to websocket server.');
@@ -27,45 +36,13 @@ const ToggleConnect = () => {
                 console.log('Connection to websocket server closed.');
               });
               
-              var listener = new ROSLIB.Topic({
-                ros : ros,
-                name : '/listener',
-                messageType : 'std_msgs/String'
-              });
-            
-            listener.subscribe(function(message) {
-                console.log('Received message on ' + listener.name + ': ' + message.data);
-                listener.unsubscribe();
-              });
             }}>Toggle Connect</button>  <br />
 
             <button onClick={() => {
-              var ros = new ROSLIB.Ros({
-                url : 'ws://localhost:9090'
-              });
-              let image_topic = new ROSLIB.Topic({
-                ros: ros, 
-                name: '/stream1/image/compressed',
-                messageType: 'sensor_msgs/CompressedImage'
-              });
-              image_topic.subscribe(function(message) {
-                document.getElementById('my_image').src = "data:image/jpg;base64," + message.data;
-                image_topic.unsubscribe();
-              });
-            }}>Click for New Image</button>
+              image_topic.unsubscribe()
+            }}>Toggle Camera Feed</button>
 
             <img alt='placeholder for ASBIR' id="my_image" src="assets/img/placeholder.png"></img>
-
-          {/* <p>
-            <b>Simple connect:  </b><button onClick={() =>{
-              toggleConnection()
-              console.log(url)
-            }}>Toggle Connect</button>  <br />
-            <b>ROS url input:  </b><input name="urlInput" defaultValue={ url } onChange={event => changeUrl(event.target.value)} />  <br />
-            <b>ROS url to connect to:  </b> {url}  <br />
-            <b>Status of ROS:</b> { isConnected ? "connected" : "not connected" }   <br />
-            <b>Topics detected:</b><br />
-            { topics.map((topic, i) => <li key={i}>    {topic.path}</li> )} */}
         </div>
     );
 }
